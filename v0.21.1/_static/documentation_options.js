68df80a412e969a4318a68d240dafb5b4bfec5d7
Santiago Soler <santisoler@fastmail.com>
2024-06-27 11:23:23 -0700

    Fix VERSION in documentation_options.js for v0.21.1

--- v0.21.1/_static/documentation_options.js
+++ v0.21.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.21.1.dev3+ge1c28e94c',
+    VERSION: '0.21.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
